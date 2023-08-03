1b92d4be9e2806d9442965b2260736a70503ded7
mariosyn <project.mariosyn@gmail.com>
2023-08-03 12:23:20 +0200

    Cambios en el buscador

--- main.js
+++ main.js
@@ -169,89 +169,132 @@ form.addEventListener('submit', (event) => {
     return;
   }
 
-  // Realizar la búsqueda y mostrar los resultados en el contenedor
+
+
+
+
+
+
+// Realizar la búsqueda y mostrar los resultados en el contenedor
 // Realizar la búsqueda y mostrar los resultados en el contenedor
 const results = searchForTerm(searchTerm);
 displayResults(results);
 results.forEach(result => {
-  const resultContainer = document.createElement('div');
-  resultContainer.classList.add('result-container'); // añadir la clase al contenedor
-  resultsContainer.appendChild(resultContainer);
+  // Crear un elemento div para el resultado
+  const resultContainer = document.createElement("div");
+  resultContainer.classList.add("result-container"); // añadir la clase al contenedor
 
-  const resultImage = document.createElement('img');
+  // Crear una imagen para el resultado
+  const resultImage = document.createElement("img");
   resultImage.src = result.img;
   resultImage.alt = result.modelo;
+  resultImage.classList.add("resultimg");  // Agregar la clase "resultimg" a resultImage
   resultContainer.appendChild(resultImage);
 
-  const resultName = document.createElement('h3');
+  // Crear un h3 para el nombre del resultado
+  const resultName = document.createElement("h3");
   resultName.textContent = result.nombre;
+  resultName.classList.add("resultname");  // Añadir la clase "resultname" a resultName
   resultContainer.appendChild(resultName);
 
-  const resultModel = document.createElement('p');
+  // Crear un div para las estadísticas del resultado
+  const divStats = document.createElement("div");
+  divStats.classList.add("result-divstats");
+
+  // Crear un elemento div para la imagen tipo
+  const imgtipodiv = document.createElement("div");
+  imgtipodiv.classList.add("imgtipodiv");
+
+  // Crear una imagen para el tipo
+  const imgTipo = document.createElement("img");
+  imgTipo.src = result.imgtipo;
+  imgTipo.alt = result.tipo;
+  imgTipo.classList.add("imgtipo");  // Agregar la clase a img
+  imgtipodiv.appendChild(imgTipo);
+
+  // Añadir la imagen al div de las estadísticas
+  divStats.appendChild(imgtipodiv);
+
+  // Crear un p para el modelo del resultado
+  const resultModel = document.createElement("p");
+  resultModel.classList.add("result-model");  // Agregar la clase al p
   resultModel.textContent = result.modelo;
-  resultContainer.appendChild(resultModel);
+  divStats.appendChild(resultModel);
 
-  const resultPrice = document.createElement('p');
+  // Crear un p para el precio del resultado
+  const resultPrice = document.createElement("p");
+  resultPrice.classList.add("result-price");  // Agregar la clase al p
   resultPrice.textContent = result.precio;
-  resultContainer.appendChild(resultPrice);
+  divStats.appendChild(resultPrice);
 
-  const resultidbici = document.createElement('p');
+  // Crear un p para el #id del resultado
+  const resultidbici = document.createElement("p");
+  resultidbici.classList.add("result-id");  // Agregar la clase al p
   resultidbici.textContent = result.id;
-  resultContainer.appendChild(resultidbici);
+  divStats.appendChild(resultidbici);
+  
+  // Agregar el div de estadísticas al div del resultado
+  resultContainer.appendChild(divStats);
 
-  const resultHistory = document.createElement('h4');
-  resultHistory.textContent = result.historia; // Toma el valor de historia del objeto JSON.
+  // Crear un h4 para la historia del resultado
+  const resultHistory = document.createElement("h4");
+  resultHistory.innerHTML = result.historia; // Toma el valor de historia del objeto JSON.
   resultContainer.appendChild(resultHistory);
 
   // Crear el div que va a contener el botón
   const botonDiv = document.createElement("div");
-  botonDiv.classList.add("divboton");  // Agregar la clase al div
+  botonDiv.classList.add("resultdivboton");  // Agregar la clase al div
 
   // Crear el botón con todo el contenido HTML que necesitas
-  const bicicletaBoton = document.createElement("button");
-  bicicletaBoton.classList.add("button");
-  bicicletaBoton.innerHTML = `
-    <span class="button__text">
-        <span>B</span><span>u</span><span>y</span><span> </span><span>N</span><span>F</span><span>T</span>
-    </span>
-    <svg class="button__svg" role="presentational" viewBox="0 0 600 600">
-                          <defs>
-                            <clipPath id="myClip">
-                              <rect x="0" y="0" width="100%" height="50%" />
-                            </clipPath>
-                          </defs>
-                          <g clip-path="url(#myClip)">
-                            <g id="money">
-                              <path d="M441.9,116.54h-162c-4.66,0-8.49,4.34-8.62,9.83l.85,278.17,178.37,2V126.37C450.38,120.89,446.56,116.52,441.9,116.54Z" fill="#699e64" stroke="#323c44" stroke-miterlimit="10" stroke-width="14" />
-                              <path d="M424.73,165.49c-10-2.53-17.38-12-17.68-24H316.44c-.09,11.58-7,21.53-16.62,23.94-3.24.92-5.54,4.29-5.62,8.21V376.54H430.1V173.71C430.15,169.83,427.93,166.43,424.73,165.49Z" fill="#699e64" stroke="#323c44" stroke-miterlimit="10" stroke-width="14" />
-                            </g>
-                            <g id="creditcard">
-                              <path d="M372.12,181.59H210.9c-4.64,0-8.45,4.34-8.58,9.83l.85,278.17,177.49,2V191.42C380.55,185.94,376.75,181.57,372.12,181.59Z" fill="#a76fe2" stroke="#323c44" stroke-miterlimit="10" stroke-width="14" />
-                              <path d="M347.55,261.85H332.22c-3.73,0-6.76-3.58-6.76-8v-35.2c0-4.42,3-8,6.76-8h15.33c3.73,0,6.76,3.58,6.76,8v35.2C354.31,258.27,351.28,261.85,347.55,261.85Z" fill="#ffdc67" />
-                              <path d="M249.73,183.76h28.85v274.8H249.73Z" fill="#323c44" />
-                            </g>
+  const resultBoton = document.createElement("button");
+  resultBoton.classList.add("button");
+  resultBoton.innerHTML = `
+  <span class="button__text">
+      <span>B</span><span>u</span><span>y</span><span> </span><span>N</span><span>F</span><span>T</span>
+  </span>
+  <svg class="button__svg" role="presentational" viewBox="0 0 600 600">
+                        <defs>
+                          <clipPath id="myClip">
+                            <rect x="0" y="0" width="100%" height="50%" />
+                          </clipPath>
+                        </defs>
+                        <g clip-path="url(#myClip)">
+                          <g id="money">
+                            <path d="M441.9,116.54h-162c-4.66,0-8.49,4.34-8.62,9.83l.85,278.17,178.37,2V126.37C450.38,120.89,446.56,116.52,441.9,116.54Z" fill="#699e64" stroke="#323c44" stroke-miterlimit="10" stroke-width="14" />
+                            <path d="M424.73,165.49c-10-2.53-17.38-12-17.68-24H316.44c-.09,11.58-7,21.53-16.62,23.94-3.24.92-5.54,4.29-5.62,8.21V376.54H430.1V173.71C430.15,169.83,427.93,166.43,424.73,165.49Z" fill="#699e64" stroke="#323c44" stroke-miterlimit="10" stroke-width="14" />
                           </g>
-                          <g id="wallet">
-                            <path d="M478,288.23h-337A28.93,28.93,0,0,0,112,317.14V546.2a29,29,0,0,0,28.94,28.95H478a29,29,0,0,0,28.95-28.94h0v-229A29,29,0,0,0,478,288.23Z" fill="#a4bdc1" stroke="#323c44" stroke-miterlimit="10" stroke-width="14" />
-                            <path d="M512.83,382.71H416.71a28.93,28.93,0,0,0-28.95,28.94h0V467.8a29,29,0,0,0,28.95,28.95h96.12a19.31,19.31,0,0,0,19.3-19.3V402a19.3,19.3,0,0,0-19.3-19.3Z" fill="#a4bdc1" stroke="#323c44" stroke-miterlimit="10" stroke-width="14" />
-                            <path d="M451.46,435.79v7.88a14.48,14.48,0,1,1-29,0v-7.9a14.48,14.48,0,0,1,29,0Z" fill="#a4bdc1" stroke="#323c44" stroke-miterlimit="10" stroke-width="14" />
-                            <path d="M147.87,541.93V320.84c-.05-13.2,8.25-21.51,21.62-24.27a42.71,42.71,0,0,1,7.14-1.32l-29.36-.63a67.77,67.77,0,0,0-9.13.45c-13.37,2.75-20.32,12.57-20.27,25.77l.38,221.24c-1.57,15.44,8.15,27.08,25.34,26.1l33-.19c-15.9,0-28.78-10.58-28.76-25.93Z" fill="#7b8f91" />
-                            <path d="M148.16,343.22a6,6,0,0,0-6,6v92a6,6,0,0,0,12,0v-92A6,6,0,0,0,148.16,343.22Z" fill="#323c44" />
+                          <g id="creditcard">
+                            <path d="M372.12,181.59H210.9c-4.64,0-8.45,4.34-8.58,9.83l.85,278.17,177.49,2V191.42C380.55,185.94,376.75,181.57,372.12,181.59Z" fill="#a76fe2" stroke="#323c44" stroke-miterlimit="10" stroke-width="14" />
+                            <path d="M347.55,261.85H332.22c-3.73,0-6.76-3.58-6.76-8v-35.2c0-4.42,3-8,6.76-8h15.33c3.73,0,6.76,3.58,6.76,8v35.2C354.31,258.27,351.28,261.85,347.55,261.85Z" fill="#ffdc67" />
+                            <path d="M249.73,183.76h28.85v274.8H249.73Z" fill="#323c44" />
                           </g>
-                      
-                        </svg>
-  `;
+                        </g>
+                        <g id="wallet">
+                          <path d="M478,288.23h-337A28.93,28.93,0,0,0,112,317.14V546.2a29,29,0,0,0,28.94,28.95H478a29,29,0,0,0,28.95-28.94h0v-229A29,29,0,0,0,478,288.23Z" fill="#a4bdc1" stroke="#323c44" stroke-miterlimit="10" stroke-width="14" />
+                          <path d="M512.83,382.71H416.71a28.93,28.93,0,0,0-28.95,28.94h0V467.8a29,29,0,0,0,28.95,28.95h96.12a19.31,19.31,0,0,0,19.3-19.3V402a19.3,19.3,0,0,0-19.3-19.3Z" fill="#a4bdc1" stroke="#323c44" stroke-miterlimit="10" stroke-width="14" />
+                          <path d="M451.46,435.79v7.88a14.48,14.48,0,1,1-29,0v-7.9a14.48,14.48,0,0,1,29,0Z" fill="#a4bdc1" stroke="#323c44" stroke-miterlimit="10" stroke-width="14" />
+                          <path d="M147.87,541.93V320.84c-.05-13.2,8.25-21.51,21.62-24.27a42.71,42.71,0,0,1,7.14-1.32l-29.36-.63a67.77,67.77,0,0,0-9.13.45c-13.37,2.75-20.32,12.57-20.27,25.77l.38,221.24c-1.57,15.44,8.15,27.08,25.34,26.1l33-.19c-15.9,0-28.78-10.58-28.76-25.93Z" fill="#7b8f91" />
+                          <path d="M148.16,343.22a6,6,0,0,0-6,6v92a6,6,0,0,0,12,0v-92A6,6,0,0,0,148.16,343.22Z" fill="#323c44" />
+                        </g>
+                    
+                      </svg>
+`;
+  botonDiv.appendChild(resultBoton);
+  resultContainer.appendChild(botonDiv);
+
+  resultsContainer.appendChild(resultContainer);
 
   // Agregar el evento al botón
-  bicicletaBoton.addEventListener("click", function() {
+  resultBoton.addEventListener("click", function() {
     window.open(result.url, "_blank");
   });
 
   // Agregar el botón al div
-  botonDiv.appendChild(bicicletaBoton);
+  botonDiv.appendChild(resultBoton);
 
   // Agregar el div al resultContainer
   resultContainer.appendChild(botonDiv);
+
 });
 
 
@@ -284,9 +327,9 @@ results.forEach(result => {
     
       // Mostrar el número de resultados encontrados
       const count = results.length;
-      const countText = count === 1 ? 'bicicleta' : 'bicicletas';
+      const countText = count === 1 ? 'shimmer' : 'shimmers';
       const countDiv = document.createElement('div');
-      countDiv.textContent = `${count} ${countText} encontrada(s).`;
+      countDiv.textContent = `${count} ${countText} encontrado(s).`;
       resultsContainer.appendChild(countDiv);
     
       
